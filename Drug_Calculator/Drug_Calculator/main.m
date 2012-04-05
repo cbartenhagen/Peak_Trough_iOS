b02438abb69b82f3758709519764776b1b1a6d6e
Jean Yoo <juminjjong@gmail.com>
2012-04-05 14:33:17 -0500

    added my sweet code

---
+++ Drug_Calculator/Drug_Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Drug_Calculator
+//
+//  Created by CS Uiowa on 4/2/12.
+//  Copyright (c) 2012 The University of Iowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Drug_CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Drug_CalculatorAppDelegate class]));
+    }
+}
